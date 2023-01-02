3c1781824c201764b54d41f0bd91c7fa2c8aa997
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2023-01-02 18:13:39 +0700

    first commit

---
+++ identity-service/src/test/java/com/gonion/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonion.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
